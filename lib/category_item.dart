b8d705610e399ec1b6fe592f99c60ef4f9ce215a
Mihail Yonchev <myonchev99@gmail.com>
2019-08-02 17:26:06 +0300

    Added Static Category Page and PageRouting from CategoriesScreen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,29 +1,47 @@
 import 'package:flutter/material.dart';
 
+import './category_meals_screen.dart';
+
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
+  final double borderRadius = 15;
 
   CategoryItem({
     @required this.title,
     @required this.color,
   });
 
+  void selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+         builder: (_) {
+           return CategoryMealsScreen();
+         }
+    ),
+    
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title, style: Theme.of(context).textTheme.title,),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(.7),
-            color,
-          ],
-          begin: Alignment.bottomLeft,
-          end: Alignment.topRight,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(borderRadius),
+          child: Container(
+        padding: EdgeInsets.all(borderRadius),
+        child: Text(title, style: Theme.of(context).textTheme.title,),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(.7),
+              color,
+            ],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
         ),
-        borderRadius: BorderRadius.circular(15),
       ),
     );
   }
