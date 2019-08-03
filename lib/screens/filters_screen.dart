8776b3900592b9b6da1d31d59d024f968f040e3d
Mihail Yonchev <myonchev99@gmail.com>
2019-08-03 18:07:40 +0300

    Added filter switches

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _glutenFree = false;
+  bool _vegeterian = false;
+  bool _vegan = false;
+
+  Widget buildSwitchListTile({String title, String subtitle, bool value}){
+    return SwitchListTile(
+                  title: Text(title),
+                  subtitle: Text(subtitle),
+                  onChanged: ((newValue) {
+                    setState(() {
+                      value = newValue;
+                    });
+                  }),
+                  value: _glutenFree,
+                );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,10 +32,39 @@ class FiltersScreen extends StatelessWidget {
         title: Text('Your Filters'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text(
-          'Filters',
-        ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                buildSwitchListTile(
+                  title: 'Gluten-free',
+                  subtitle: 'Only include gluten-free meals',
+                  value: _glutenFree,
+                ),
+                buildSwitchListTile(
+                  title: 'Vegeterian',
+                  subtitle: 'Only include vegeterian meals',
+                  value: _vegeterian,
+                ),
+                buildSwitchListTile(
+                  title: 'Vegan',
+                  subtitle: 'Only include vegan meals',
+                  value: _vegan,
+                )
+                
+              
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
