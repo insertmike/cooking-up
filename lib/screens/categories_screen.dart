2bcc6ed71df563acf0e6b887170e6f52d087e3e3
Mihail Yonchev <myonchev99@gmail.com>
2019-08-03 14:50:28 +0300

    Added tabBar

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -6,25 +6,20 @@ import '../widgets/debugging_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meals'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(18),
-        children: DUMMY_CATEGORIES.map((catData) {
-          return CategoryItem(
-            id: catData.id,
-            color: catData.color,
-            title: catData.title,
-          );
-        }).toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2, // For 200 width -> 300 height
-          mainAxisSpacing: 20,
-          crossAxisSpacing: 20,
-        ),
+    return GridView(
+      padding: const EdgeInsets.all(18),
+      children: DUMMY_CATEGORIES.map((catData) {
+        return CategoryItem(
+          id: catData.id,
+          color: catData.color,
+          title: catData.title,
+        );
+      }).toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2, // For 200 width -> 300 height
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
       ),
     );
   }
